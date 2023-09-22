6a7a293ca4d000a0862722049360a6a32c58ef0b
woody <onlyhale@naver.com>
2023-09-22 18:07:20 +0900

    [Feat] sub_page goods부분 생성
    sub_page goods 부분 생성 중입니다.
    
    Resolves #1

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function App() {
   return (
     <>
       <Sub_Page/>
-      <Common_Contents/>
     </>
   );
 }
--- src/Components/Sub_Page/Make/Equipment/Equipment.js
+++ src/Components/Sub_Page/Make/Equipment/Equipment.js
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { useRef } from "react"
 import './Equipment.css'
+import Goods from './Goods'
 
-function Equipment(){ 
-  const scroll = (direction) => {
-    const scrollAmount = 800; // Adjust this value to control the scrolling speed
-    const container = document.querySelector('.slider-container');
+function Equipment(){  
+  const containerRef = useRef(null)
+  
+  const resetScroll = () => {
+    const container = containerRef.current;
     
-    if (container) {
-      if (direction === 'left') {
-        container.scrollLeft -= scrollAmount;
-      } else if (direction === 'right') {
-        container.scrollLeft += scrollAmount;
+    if (container){
+      if (container.scrollLeft === 0){
+        container.scrollTo({ left: container.scrollWidth, behavior: 'smooth' })
+      }else if(container.scrollLeft >= container.scrollWidth - container.clientWidth){
+        container.scrollTo({ left: 0, behavior: 'smooth' })
       }
     }
   }
@@ -18,36 +20,77 @@ function Equipment(){
   return(
     <div className="goods">
       <div className="goods_category">
-        <div className="slider-container">
+        <button className="slide-button left" onClick={() => resetScroll()}><h2>{"<<"}</h2></button>
+        <div className="slider-container" ref={containerRef}>
           <div className="good_category_img"><div className="view_all">전체보기</div></div>
           <div className="good_category_img">
-            <img src={require('../../../../asset/imgs/카테고리이미지(스미스짐).jpg')} alt="스미스짐" />
-            <div className="asd"><h1>스미스</h1></div>
+            <img src={require('../../../../asset/imgs/category/카테고리이미지(스미스짐).jpg')} alt="스미스짐" />
+            <div className="img_text"><h1>스미스짐</h1></div>
           </div>          
-          <div className="good_category_img"><img src={require('../../../../asset/imgs/카테고리이미지(멀티짐).jpg')} alt="멀티짐" /></div>          
-          <div className="good_category_img"><img src={require('../../../../asset/imgs/카테고리이미지(멀티랙).jpg')} alt="멀티랙" /></div>          
-          <div className="good_category_img"><img src={require('../../../../asset/imgs/카테고리이미지(벤치).jpg')} alt="벤치" /></div>          
-          <div className="good_category_img"><img src={require('../../../../asset/imgs/카테고리이미지(벤치프레스).jpg')} alt="벤치프레스" /></div>          
-          <div className="good_category_img"><img src={require('../../../../asset/imgs/카테고리이미지(런닝머신).jpg')} alt="런닝머신" /></div>          
-          <div className="good_category_img"><img src={require('../../../../asset/imgs/카테고리이미지(사이클).jpg')} alt="사이클" /></div>          
-          <div className="good_category_img"><img src={require('../../../../asset/imgs/카테고리이미지(로잉머신).jpg')} alt="로잉머신" /></div>          
-          <div className="good_category_img"><img src={require('../../../../asset/imgs/카테고리이미지(일립티컬).jpg')} alt="일립티컬" /></div>          
-          <div className="good_category_img"><img src={require('../../../../asset/imgs/카테고리이미지(복싱).jpg')} alt="복싱" /></div>          
-          <div className="good_category_img"><img src={require('../../../../asset/imgs/카테고리이미지(스미스짐).jpg')} alt="스미스짐" /></div>          
-          <div className="good_category_img"><img src={require('../../../../asset/imgs/카테고리이미지(멀티짐).jpg')} alt="멀티짐" /></div>          
-          <div className="good_category_img"><img src={require('../../../../asset/imgs/카테고리이미지(멀티랙).jpg')} alt="멀티랙" /></div>          
-          <div className="good_category_img"><img src={require('../../../../asset/imgs/카테고리이미지(벤치).jpg')} alt="벤치" /></div>          
-          <div className="good_category_img"><img src={require('../../../../asset/imgs/카테고리이미지(벤치프레스).jpg')} alt="벤치프레스" /></div>          
-          <div className="good_category_img"><img src={require('../../../../asset/imgs/카테고리이미지(런닝머신).jpg')} alt="런닝머신" /></div>          
-          <div className="good_category_img"><img src={require('../../../../asset/imgs/카테고리이미지(사이클).jpg')} alt="사이클" /></div>          
-          <div className="good_category_img"><img src={require('../../../../asset/imgs/카테고리이미지(로잉머신).jpg')} alt="로잉머신" /></div>          
-          <div className="good_category_img"><img src={require('../../../../asset/imgs/카테고리이미지(일립티컬).jpg')} alt="일립티컬" /></div>          
-          <div className="good_category_img"><img src={require('../../../../asset/imgs/카테고리이미지(복싱).jpg')} alt="복싱" /></div> 
-        </div>
-        <button className="slide-button left" onClick={() => scroll('left')}>←</button>
-        <button className="slide-button right" onClick={() => scroll('right')}>→</button>
+          <div className="good_category_img">
+            <img src={require('../../../../asset/imgs/category/카테고리이미지(멀티짐).jpg')} alt="멀티짐" />
+            <div className="img_text"><h1>멀티짐</h1></div>
+          </div>          
+          <div className="good_category_img">
+            <img src={require('../../../../asset/imgs/category/카테고리이미지(멀티랙).jpg')} alt="멀티랙" />
+            <div className="img_text"><h1>멀티랙</h1></div>
+          </div>          
+          <div className="good_category_img">
+            <img src={require('../../../../asset/imgs/category/카테고리이미지(벤치).jpg')} alt="벤치" />
+            <div className="img_text"><h1>벤치</h1></div>
+          </div>          
+          <div className="good_category_img">
+            <img src={require('../../../../asset/imgs/category/카테고리이미지(벤치프레스).jpg')} alt="벤치프레스" />
+            <div className="img_text"><h1>벤치프레스</h1></div>
+          </div>          
+          <div className="good_category_img">
+            <img src={require('../../../../asset/imgs/category/카테고리이미지(런닝머신).jpg')} alt="런닝머신" />
+            <div className="img_text"><h1>런닝머신</h1></div>
+          </div>          
+          <div className="good_category_img">
+            <img src={require('../../../../asset/imgs/category/카테고리이미지(사이클).jpg')} alt="사이클" />
+            <div className="img_text"><h1>사이클</h1></div>
+          </div>          
+          <div className="good_category_img">
+            <img src={require('../../../../asset/imgs/category/카테고리이미지(로잉머신).jpg')} alt="로잉머신" />
+            <div className="img_text"><h1>로잉머신</h1></div>
+          </div>          
+          <div className="good_category_img">
+            <img src={require('../../../../asset/imgs/category/카테고리이미지(일립티컬).jpg')} alt="일립티컬" />
+            <div className="img_text"><h1>일립티컬</h1></div>
+          </div>          
+          <div className="good_category_img">
+            <img src={require('../../../../asset/imgs/category/카테고리이미지(복싱).jpg')} alt="복싱" />
+            <div className="img_text"><h1>복싱</h1></div>
+          </div> 
+          <div className="good_category_img">
+            <img src={require('../../../../asset/imgs/category/카테고리이미지(런닝머신).jpg')} alt="런닝머신" />
+            <div className="img_text"><h1>런닝머신</h1></div>
+          </div>          
+          <div className="good_category_img">
+            <img src={require('../../../../asset/imgs/category/카테고리이미지(사이클).jpg')} alt="사이클" />
+            <div className="img_text"><h1>사이클</h1></div>
+          </div>          
+          <div className="good_category_img">
+            <img src={require('../../../../asset/imgs/category/카테고리이미지(로잉머신).jpg')} alt="로잉머신" />
+            <div className="img_text"><h1>로잉머신</h1></div>
+          </div>          
+          <div className="good_category_img">
+            <img src={require('../../../../asset/imgs/category/카테고리이미지(일립티컬).jpg')} alt="일립티컬" />
+            <div className="img_text"><h1>일립티컬</h1></div>
+          </div>          
+          <div className="good_category_img">
+            <img src={require('../../../../asset/imgs/category/카테고리이미지(복싱).jpg')} alt="복싱" />
+            <div className="img_text"><h1>복싱</h1></div>
+          </div>              
+        </div>        
+        <button className="slide-button right" onClick={() => resetScroll()}><h2>{">>"}</h2></button>
       </div>
-      <div className="goods_area"></div>
+      <div className="goods_area">
+        <div className="menu_goods">
+          <Goods/>
+        </div>
+      </div>
     </div>
   )
 }
