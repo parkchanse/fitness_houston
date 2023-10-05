fb31497c2cc2847a736f74c2c91f244b46bafd26
User -2023XQKFO\Administrator <wj402@naver.com>
2023-10-05 10:26:37 +0900

    [Feat] Article03 구현 완료
    기구 소개 영역 구현 완료했습니다. 슬라이드 기능은 아직 구현하지 못했습니다.
    
    Resolves:#1

--- src/Components/Main_Page/Main_Page.js
+++ src/Components/Main_Page/Main_Page.js
@@ -2,6 +2,7 @@ import './Main_page.css'
 import Main from './Main/Main'
 import Article01 from './Article01/Article01'
 import Article02 from './Article02/Article02'
+import Article03 from './Article03/Article03'
 
 function Main_Page() {
     return (
@@ -9,6 +10,7 @@ function Main_Page() {
             <Main />
             <Article01 />
             <Article02 />
+            <Article03 />
         </div>
     )
 }
