2a3a4e207e798cc7255aa64f7fb62169b5c87e38
woody <onlyhale@naver.com>
2023-10-10 18:00:24 +0900

    [Feat] 로그인 페이지 및 반응형 완료
    로그인페이지 및 반응형 완료했습니다.
    
    Resolves : #1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-
+/* eslint-disable */
 import './asset/css/reset.css'
 import './asset/css/App.css';
+import Main_Page from './Components/Main_Page/Main_Page';
+import Common_Contents from './Components/Common_Contents/Common_Contents'
+import Sub_Page from './Components/Sub_Page/Sub_Page'
 
 function App() {
   return (
     <>
+      <Sub_Page/>
     </>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/Sub_Page/Sub_Page.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './Sub_Page.css'
+// import ConHead from "./Make/Head/ConHead";
+// import ConBanner from "./Make/Banner/ConBanner";
+// import Equipment from './Make/Equipment/Equipment'
+import Login from "./Login/Login";
+
+
+function Sub(){
+  return(
+    <div className="Sub_Page">
+      {/* <ConHead/> */}
+      {/* <ConBanner/>   */}
+      {/* <Equipment/>     */}
+      <Login/>
+    </div>
+  )
+}
+
+export default Sub
