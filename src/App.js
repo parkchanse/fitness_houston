7461b83e8b37a51d04c83ac63c69794411888508
User -2023XQKFO\Administrator <wj402@naver.com>
2023-10-16 17:42:03 +0900

    빌드 테스트

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 /* eslint-disable */
 
+import React from 'react';
 import './asset/css/reset.css'
 import './asset/css/App.css';
 import Main_Page from './Components/Main_Page/Main_Page';
 import Common_Contents from './Components/Common_Contents/Common_Contents'
 import Sub_Page from './Components/Sub_Page/Sub_Page'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './Components/Sub_Page/Login/Login';
+import Map from './Components/Common_Contents/Map/Map'
+import Condition from './Components/Sub_Page/Login/Condition';
+
 
 function App() {
   return (
     <div>
-      <Main_Page/>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <Routes>
+          <Route path="/" element={<Main_Page />} />
+          <Route path="sub" element={<Sub_Page />} />
+          <Route path="announcement" element={<Common_Contents />} />
+          <Route path="login" element={<Login />} />
+          <Route path="condition" element={<Condition />}/>
+          <Route path="map" element={<Map />}/>
+
+        </Routes>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
 export default App;
